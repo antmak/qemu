fd0082494d295069fd63b2a6b5872ac3a019c818
Anton Maklakov <anton@espressif.com>
2023-12-05 15:26:21 +0700

    macos fix 7

--- .github/workflows/scripts/prerequisites-macos.sh
+++ .github/workflows/scripts/prerequisites-macos.sh
@@ -14,6 +14,11 @@ brew install \
 # workaround if deprecated module 'distutils.version' is missing
 # https://peps.python.org/pep-0632/#migration-advice
 # https://pypi.org/project/looseversion/
+#
+# Used for building only, not for qemu run-time
+#
+# Also if something goes wrong, 'setup-python' action can be used
+# as a general solution, https://github.com/actions/runner-images/issues/8932#issuecomment-1836013315)
 PYFIX_FILE=/usr/local/Cellar/glib/2.78.1/share/glib-2.0/codegen/utils.py
 if [ -f "${PYFIX_FILE}" ] ; then
   python3 -m pip install --upgrade pip
